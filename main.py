60e1fa7337d2273f9dfa3879d5a4dc44918fe1f5
Hamza Ali Khan <drstoneeren@gmail.com>
2024-10-15 23:20:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -55,10 +55,10 @@ logging.basicConfig(level=logging.INFO)
 
 
 def get_db_connection():
-    server = "dhpdevazsql01.database.windows.net"
-    database = "dhpdevdb01"
-    username = "dhpdevwebappdb01"
-    password = "Z.M!EpvtWa!i233zq!pzyNP4Hkn4*CEKeeCwkcv4C"
+    server = os.getenv("DATABASE_SERVER")
+    database = os.getenv("DATABASE_NAME")
+    username = os.getenv("DATABASE_USERNAME")
+    password = os.getenv("DATABASE_PASSWORD")
     driver = "{ODBC Driver 17 for SQL Server}"
 
     connection_string = f"DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}"
